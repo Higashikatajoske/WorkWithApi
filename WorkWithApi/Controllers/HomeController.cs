b8b98144636b69383f8d84c9671ce92b3e2c4d58
Nikolay Samus <2345977@gmail.com>
2021-10-31 17:08:59 +0700

    Добавьте файлы проекта.

---
+++ WorkWithApi/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WorkWithApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
+using System.Data;
+
+namespace WorkWithApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly IHttpClientFactory httpClientFactory;
+        private static HttpClient httpClient;
+        const string requestTokenUrl = "oauth/requesttoken";
+        const string accTokenUrlTemp = "oauth/accesstoken";
+        const string orderUrlTemp = "orders";
+
+        public HomeController(IHttpClientFactory clientFactory)
+        {
+            httpClientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            Tokens token;
+            var request = new HttpRequestMessage(HttpMethod.Get, requestTokenUrl);
+            httpClient = httpClientFactory.CreateClient("pixlPark");
+
+            var response = await httpClient.SendAsync(request);
+            string jsonAnswer = response.Content.ReadAsStringAsync().Result;
+            token = JsonConvert.DeserializeObject<Tokens>(jsonAnswer);
+
+            request = new HttpRequestMessage(HttpMethod.Get, UrlCreator(token, false));
+            response = await httpClient.SendAsync(request);
+            jsonAnswer = response.Content.ReadAsStringAsync().Result;
+            token.RefreshToken = JsonConvert.DeserializeObject<Tokens>(jsonAnswer).RefreshToken;
+            token.AccessToken = JsonConvert.DeserializeObject<Tokens>(jsonAnswer).AccessToken;
+
+            request = new HttpRequestMessage(HttpMethod.Get, UrlCreator(token, true));
+            response = await httpClient.SendAsync(request);
+            JObject json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            JToken jsonResult = json["Result"];
+            ViewData["Dictionary"] = new Dictionary<string, JToken>();
+
+            return View(jsonResult);
+            
+        }
+
+        private string UrlCreator(Tokens tokens, bool isOrders = true)
+        {
+            string url = "";
+            if (isOrders)
+            {
+                url = orderUrlTemp + "?oauth_token=" + tokens.AccessToken;
+            }
+            else
+            {
+                url = accTokenUrlTemp + "?oauth_token=" + tokens.RequestToken + "&grant_type=api&username=" +
+                     Tokens.PublicKey + "&password=" + Hash(tokens.RequestToken);
+            }
+            return url;
+        }
+
+        private string Hash(string requestToken)
+        {
+            string unhashedPassw = requestToken + Tokens.PrivateKey;
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(unhashedPassw));
+                StringBuilder sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+
+    }
+}
---
+++ WorkWithApi/Models/Tokens.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WorkWithApi.Models
+{
+    public class Tokens
+    {
+        [JsonPropertyName("RequestToken")]
+        public string RequestToken { get; set; }
+        [JsonPropertyName("AccessToken")]
+        public string AccessToken { get; set; }
+        [JsonPropertyName("RefreshToken")]
+        public string RefreshToken { get; set; }
+        public static string PublicKey = "38cd79b5f2b2486d86f562e3c43034f8";
+        public static string PrivateKey = "8e49ff607b1f46e1a5e8f6ad5d312a80";
+
+        public Tokens()
+        {
+        }
+
+        public Tokens (string reqToken)
+        {
+            RequestToken = reqToken;
+        }
+
+        public Tokens(string reqToken, string accessTok)
+        {
+            RequestToken = reqToken;
+            AccessToken = accessTok;
+        }
+    }
+}
